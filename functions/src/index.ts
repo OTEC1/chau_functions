cb899c5b26be6ef485cbb02050d9fcaf6ab9f5e3
Tobe <gregokko18@gmail.com>
2021-11-30 22:39:21 +0100

    added cors dependency

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,8 @@ import * as express from "express";
 import {getAllResquest} from "./config/model_data";
 import {banklistapi} from "./config/model_data";
 import {foodcategory} from "./config/model_data";
+import * as corsmodule from "cors";
+const cors = corsmodule(({origin: true}));
 
 // can make multiply calls  from within appCat block
 const appCat = express();
@@ -13,6 +15,8 @@ exports.appCat = functions.https.onRequest(appCat);
 
 // date
 export const getTimeStamp = functions.https.onRequest((request, respones) => {
-  respones.setHeader("Content-Type", "application/json");
-  respones.send(JSON.stringify({timestamp: Date.now()}));
+  cors(request, respones, async () => {
+    respones.setHeader("Content-Type", "application/json");
+    respones.send(JSON.stringify({timestamp: Date.now()}));
+  });
 });
